67f74037ab34a300157ec96510346ad205ccf9e3
Lukas Vinclav <lukas.vinclav@gmail.com>
2012-11-01 16:10:30 +0100

    setuptools

--- setup.py
+++ setup.py
@@ -1,25 +1,18 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='django-flatpages-i18n',
     version='0.1.0',
     description='Translatable flatpages',
-    long_description=(),
     author='Pragmatic Mates',
     author_email='info@pragmaticmates.com',
     maintainer='Pragmatic Mates',
     maintainer_email='info@pragmaticmates.com',
     url='https://github.com/PragmaticMates/django-flatpages-i18n',
     packages=['flatpages_i18n'],
-    package_data={},
-    requires=[
-        'South',
-        'django_modeltranslation',
-        'django(>=1.3)'
-    ],
-    download_url='https://github.com/downloads/deschler/django-modeltranslation/django-modeltranslation-0.4.0-beta2.tar.gz',
+    install_requires=('South', 'django_modeltranslation', 'django'),
     classifiers=[
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.5',
