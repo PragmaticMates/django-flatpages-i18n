61fe1cd0854807362bdfb4105bf9d7d760640a09
Lukas Vinclav <lukas.vinclav@gmail.com>
2013-02-11 08:12:53 +0100

    unicode error fix

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='django-flatpages-i18n',
-    version='0.1.0',
+    version='0.1.2',
     description='Translatable flatpages',
     author='Pragmatic Mates',
     author_email='info@pragmaticmates.com',
