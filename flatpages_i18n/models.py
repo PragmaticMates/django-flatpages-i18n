e562744defb47fbf196f2e7fa7704fd180a04d54
Warnar Boekkooi <warnar@boekkooi.net>
2014-02-04 11:20:52 +0800

    Add BooleanField default value
    Django 1.6 changed the default value of BooleanField from False to None. 
    This patch avoids warnings in django master branche

--- flatpages_i18n/models.py
+++ flatpages_i18n/models.py
@@ -21,7 +21,7 @@ class FlatPage_i18n(MPTTModel):
         help_text=_(u"Example: 'flatpages_i18n/contact_page.html'. If this isn't \
         provided, the system will use 'flatpages_i18n/default.html'."))
     registration_required = models.BooleanField(
-        _(u'registration required'),
+        _(u'registration required'), default=False,
         help_text=_(u"If this is checked, only logged-in users will be able \
         to view the page."))
     weight = models.IntegerField(
