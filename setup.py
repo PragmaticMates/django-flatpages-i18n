b63e194b4936f6253830743e700fd56f6a3224df
eriktelepovsky <erik.telepovsky@gmail.com>
2022-07-14 16:25:02 +0200

    v1.1.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='django-flatpages-i18n',
-    version='1.1.0',
+    version='1.1.1',
     description='Translatable flatpages',
     long_description=open('README.rst').read(),
     author='Pragmatic Mates',
